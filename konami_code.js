c8cdf8f3ff18c0096e02df09865e1c4047bfb8c0
Michael Soares <mikey.soares@gmail.com>
2018-10-24 13:18:47 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,19 @@ const codes = [
 
 function init() {
   // your code here
-}
+
+function konamiListener(e) {
+   const key = e.key
+   console.log(key)
+    if (key === codes[index]) {
+     index++
+   } else {
+     index = 0
+   }
+    if (index === codes.length) {
+     alert("yes")
+   }
+    console.log(index)
+ }
+ document.body.addEventListener('keydown', konamiListener)
+ }
